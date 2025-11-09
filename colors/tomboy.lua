618b5d9208ef77169aafd5110cd996e90d2b90f5
rootankush <rootankushprompt@gmail.com>
2025-11-09 16:14:56 +0530

    Initial commit: tomboy.nvim colorscheme

---
+++ colors/tomboy.lua
@@ -0,0 +1,1 @@
+require("tomboy.nvim").setup()
---
+++ lua/tomboy.nvim/init.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.setup()
+
+    local colors = {
+        bg = "#000000",
+        fg = "#FFFFFF",
+    }
+
+end
+
+return M
